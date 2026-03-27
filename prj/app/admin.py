4996a86b6a5ccc1a0a6fa2aa7c86a7769a3effb6
Adam Rexa <adam.rexa.s@gyarab.cz>
2026-03-27 01:13:30 +0100

    Update admin.py

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import User, Settings, Taktika, Kategorie
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("name", "minecraft_username", "role")
+    search_fields = ("name", "minecraft_username")
+    list_filter = ("role",)
+
+
+@admin.register(Settings)
+class SettingsAdmin(admin.ModelAdmin):
+    list_display = ("user",)
+    search_fields = ("user__name",)
+
+
+@admin.register(Kategorie)
+class KategorieAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Taktika)
+class TaktikaAdmin(admin.ModelAdmin):
+    list_display = ("name", "difficulty", "effectivity", "usefulness", "category", "author")
+    list_filter = ("category", "difficulty")
+    search_fields = ("name", "author__name")
