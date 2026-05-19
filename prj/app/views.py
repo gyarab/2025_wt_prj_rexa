c346afbf45c890a2e69c9bf3d5c3cd728862daee
Adam Rexa <adam.rexa.s@gyarab.cz>
2026-05-19 22:29:53 +0200

    Update views.py

--- prj/app/views.py
+++ prj/app/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Taktika
 
-# Create your views here.
+
+# HTML stránky
+
 def render_homepage(request):
-	return render(request, "home.html")
+    return render(request, "home.html")
+
 
 def render_about(request):
-	return render(request, "about.html")
+    return render(request, "about.html")
+
+
+def render_api_playground(request):
+    return render(request, "api_playground.html")
+
+
+# API endpoint
+
+def taktiky_api(request):
+    taktiky = Taktika.objects.all()
+
+    data = []
+
+    for taktika in taktiky:
+        data.append({
+            "id": taktika.id,
+            "name": taktika.name,
+            "difficulty": taktika.difficulty,
+            "effectivity": taktika.effectivity,
+            "usefulness": taktika.usefulness,
+        })
+
+    return JsonResponse(data, safe=False)
